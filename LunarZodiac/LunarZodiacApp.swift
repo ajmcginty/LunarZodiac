bef3fb4352e33331ed16a1af0960152ff2af60ea
Aidan McGinty <ajmcginty1@gmail.com>
2026-02-02 17:44:16 -0500

    Initial Commit

---
+++ LunarZodiac/LunarZodiacApp.swift
@@ -0,0 +1,17 @@
+//
+//  LunarZodiacApp.swift
+//  LunarZodiac
+//
+//  Created by Aidan McGinty on 2/2/26.
+//
+
+import SwiftUI
+
+@main
+struct LunarZodiacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
